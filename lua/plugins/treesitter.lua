fc6bca02c3a3a14801b07d0a31685a14f02a3a08
Ian Cates-Doglio <icates-doglio@teainc.org>
2025-05-31 18:52:11 -0700

    feat(ts): Control enter to send the relevant block of text to the repl

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -90,6 +90,7 @@ return {
     -- 💡 Flat statement stepper
     local ts = vim.treesitter
     local ts_utils = require("nvim-treesitter.ts_utils")
+    local ts_select = require("nvim-treesitter.textobjects.select")
 
     local function node_depth(node)
       local depth = 0
@@ -137,7 +138,87 @@ return {
       print("No next flat statement found.")
     end
 
-    -- ⌨️ Bind custom flat-stepper to "]v"
     vim.keymap.set("n", "]v", goto_next_flat_statement, { desc = "Next flat statement (same depth)" })
+
+    local function visual_selection()
+      local _, ls, cs = unpack(vim.fn.getpos("'<")) -- Start position
+      local _, le, ce = unpack(vim.fn.getpos("'>")) -- End position
+
+      -- Convert from 1-based to 0-based indexing
+      ls, cs = ls - 1, cs - 1
+      le, ce = le - 1, ce
+
+      local lines = vim.api.nvim_buf_get_text(0, ls, cs, le, ce, {})
+      return lines
+    end
+
+    -- Format table of text to python repl format:
+    local function format_to_python(tab)
+      local leading_whitespace = tab[1]:match("^(%s*)%S")
+
+      if leading_whitespace == nil then
+        return tab
+      end
+      leading_whitespace = #leading_whitespace
+
+      for i, line in ipairs(tab) do
+        tab[i] = line:sub(leading_whitespace + 1)
+      end
+
+      return tab
+    end
+
+    -- overwrite existing register with python repl formatting
+    local function format_reg_to_python(reg)
+      local lines = format_to_python(vim.fn.getreg(reg, 1, true))
+      local out = table.concat(lines, "\n")
+
+      -- vim.notify("Formatted register " .. reg .. " for python repl")
+      vim.fn.setreg(reg, out)
+      return reg
+    end
+
+    -- Sends a table of text to a generic wezterm pane
+    local function send_to_repl(tab)
+      local text = table.concat(tab, "\r")
+      if vim.env.TERM_PROGRAM == "WezTerm" then
+        local handle = io.popen("wezterm cli get-pane-direction right")
+        local pane_id = handle:read("*a"):gsub("%s+", "")
+        handle:close()
+        vim.system({
+          "wezterm", "cli", "send-text",
+          "--pane-id", pane_id,
+          text .. "\r",
+          "--no-paste"
+        })
+      else
+        local file_loc = CONF .. "/nvim/ahk_scripts/send_to_repl.ahk"
+        vim.fn.setreg("+", text) -- put it in clipboard register
+        os.execute('start "" Autohotkey "' .. file_loc .. '"' .. text)
+      end
+    end
+
+    local function select_statement_and_yank(query, reg)
+      ts_select.select_textobject(query, "textobjects", "o")
+      vim.cmd('normal! "' .. reg .. "y")
+    end
+
+    local function feedkeys(keys, mode)
+      local termkeys = vim.api.nvim_replace_termcodes(keys, true, false, true)
+      vim.api.nvim_feedkeys(termkeys, mode, false)
+    end
+
+    -- Execute control enter send
+    vim.keymap.set("n", "<C-h>", function()
+      -- setup special register
+      local register = "z"
+
+      select_statement_and_yank("@statement.outer", register)
+
+      local lines = format_to_python(vim.fn.getreg(register, 1, true))
+
+      send_to_repl(lines)
+      goto_next_flat_statement()
+    end)
   end
 }
